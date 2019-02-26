f1e67de78e8b5772401e46e772ccc3ee7e1d9cff
fatpandaboy <slam19@deerfield.edu>
2019-02-25 22:53:09 -0500

    first commit

--- BinarySearchTree/SymbolTableAnalysis.h
+++ BinarySearchTree/SymbolTableAnalysis.h
@@ -8,6 +8,18 @@
 
 #ifndef SymbolTableAnalysis_h
 #define SymbolTableAnalysis_h
+#import "BSTree.h"
+#import "BinarySearch.h"
+
+@interface SymbolTableAnalysis <ObjectType>: NSObject
+
+@property BSTree* myTree;
+@property int* BSArray;
+@property int key;
+@property int N;
+
+-(void) compareRunTime;
 
 
+@end
 #endif /* SymbolTableAnalysis_h */

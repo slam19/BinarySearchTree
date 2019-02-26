ae533bdb2a5c9d79bcfb9aceabf8d9b72c6f0b5d
fatpandaboy <slam19@deerfield.edu>
2019-02-25 22:50:17 -0500

    first commit

--- BinarySearchTree/BSTree.h
+++ BinarySearchTree/BSTree.h
@@ -8,6 +8,30 @@
 
 #ifndef BSTree_h
 #define BSTree_h
+#import "Node.h"
+
+@interface BSTree: NSObject
+
+@property Node *root;
+//@property int count; 
+
+-(id) init;
+-(bool) insert: (int) i;
+-(bool) insert: (int) i at:(Node *) n;
+-(void) loadTree: (NSString *) source;
+-(void) printPreOrder: (Node *) n;
+-(void) printInOrder: (Node *) n;
+-(void) printPostOrder: (Node *) n;
+-(int) size: (Node *) n;
+-(Node *) remove: (int) i at:(Node *)n;
+-(bool) contains: (int) i at:(Node *) n;
+-(int) sum: (Node *)n;
+-(int) countLeaves: (Node *)n;
+-(int) maxPathSum: (Node *)n;
+-(int) height: (Node *) n;
+-(int) successorOfNode: (Node *) n;
+
+@end
 
 
 #endif /* BSTree_h */
